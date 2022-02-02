8fb5aa07fda07cf4a4caf2b6f2494a097f57c31f
Malder82Fox <pto.nz@yandex.ru>
2022-02-02 17:47:35 +0300

    create new code in HW_14

--- hw_14.py
+++ hw_14.py
@@ -1,25 +1,42 @@
-a = int(input('Enter Your number: '))
-b = a // 10
-c = b // 10
-d = c // 10
-e = d // 10
-f = e // 10
-g = f // 10
-h = g // 10
-i = h // 10
+# a = int(input('Enter Your number: '))
+# b = a // 10
+# c = b // 10
+# d = c // 10
+# e = d // 10
+# f = e // 10
+# g = f // 10
+# h = g // 10
+# i = h // 10
+#
+# a_1 = i // 10
+# a_2 = i % 10
+# a_3 = h // 10
+# a_4 = g // 10
+# a_5 = f // 10
+# a_6 = e // 10
+# a_7 = d // 10
+# a_8 = c // 10
+# a_9 = b // 10
+# a_0 = a // 10
+#
+# num = [a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_0]
+#
+# print('Max digit in this number - it is: ', max(num))
+# print(a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_0)
 
-a_1 = i // 10
-a_2 = i % 10
-a_3 = h // 10
-a_4 = g // 10
-a_5 = f // 10
-a_6 = e // 10
-a_7 = d // 10
-a_8 = c // 10
-a_9 = b // 10
-a_0 = a // 10
+num_init = int(input('enter number: '))
+maximum = num_init % 10
+num = num_init
 
-num = [a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_0]
+while num > 0:
+    digit = num % 10
+    if digit > maximum:
+        maximum = digit
 
-print('Max digit in this number - it is: ', max(num))
-print(a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_0)
+    if digit == 9:
+        break
+
+    num //= 10
+    print(num)
+
+print(f"The largest digit in a number {num_init} it is {maximum}")

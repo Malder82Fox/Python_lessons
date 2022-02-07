f28a04bf189132de3f263301a20703c0da1b8635
Malder82Fox <pto.nz@yandex.ru>
2022-02-07 23:54:57 +0300

    begin lesson_3

---
+++ ex_31.py
@@ -0,0 +1,13 @@
+def print_hello(): # Это функция
+    print('Hello Word !!!')
+
+print_hello()
+
+def summa(a, b):
+    result = a + b
+    print(result)
+
+summa(10, 5)
+
+
+
---
+++ ex_32.py
@@ -0,0 +1,11 @@
+# def summa(a, b):
+#     result = a + b
+#     return result # данная команда завершает функцию и выводит результат
+#
+# res = summa(10, 5)
+# print(res)
+
+def summa(a, b):
+    return a + b
+
+a = summa

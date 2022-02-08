4f0900c36b79fd5c7a569bf2b985f93f5b88a97c
Malder82Fox <pto.nz@yandex.ru>
2022-02-08 16:37:54 +0300

    made firs qestion

---
+++ hw_21.py
@@ -0,0 +1,16 @@
+ex_1 = list('Hello')
+print(ex_1)
+
+ex_2 = list([234, 456, 876])
+print(ex_2)
+
+ex_3 = list([234, 432,567, 'Hello'])
+print(ex_3)
+
+ex_4 = list([[321, 345, 567], 'Hello', 42.2])
+print(ex_4)
+
+ex_5 = list([324, 456,654, 'Hello', 34.4, None])
+print(ex_5)
+
+
